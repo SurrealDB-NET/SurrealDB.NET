01a7689b9f1974ff8dc62bfbb6520d61b88a5bdc
alexy4744 <alexy4744@gmail.com>
2022-12-29 22:18:14 -0500

    add quotation marks for crypto functions

--- src/SurrealDB.QueryBuilder/Functions/CryptoFunction.cs
+++ src/SurrealDB.QueryBuilder/Functions/CryptoFunction.cs
@@ -2,26 +2,42 @@ namespace SurrealDB.QueryBuilder.Functions;
 
 public static class CryptoFunction
 {
-    public static string Md5(object value) => $"crypto::md5({value})";
-    public static string Sha1(object value) => $"crypto::sha1({value})";
-    public static string Sha256(object value) => $"crypto::sha256({value})";
-    public static string Sha512(object value) => $"crypto::sha512({value})";
+    public static string Md5(string value)
+        => $"crypto::md5('{value}')";
+
+    public static string Sha1(string value)
+        => $"crypto::sha1('{value}')";
+
+    public static string Sha256(string value)
+        => $"crypto::sha256('{value}')";
+
+    public static string Sha512(string value)
+        => $"crypto::sha512('{value}')";
 
     public static class Argon2
     {
-        public static string Compare(object hash, object plainText) => $"crypto::argon2::compare({hash}, {plainText})";
-        public static string Generate(object value) => $"crypto::argon2::generate({value})";
+        public static string Compare(string hash, string plainText)
+            => $"crypto::argon2::compare('{hash}', '{plainText}')";
+
+        public static string Generate(string value)
+            => $"crypto::argon2::generate('{value}')";
     }
 
     public static class Pbkdf2
     {
-        public static string Compare(object hash, object plainText) => $"crypto::pbkdf2::compare({hash}, {plainText})";
-        public static string Generate(object value) => $"crypto::pbkdf2::generate({value})";
+        public static string Compare(string hash, string plainText)
+            => $"crypto::pbkdf2::compare('{hash}', '{plainText}')";
+
+        public static string Generate(string value)
+            => $"crypto::pbkdf2::generate('{value}')";
     }
 
     public static class Scrypt
     {
-        public static string Compare(object hash, object plainText) => $"crypto::scrypt::compare({hash}, {plainText})";
-        public static string Generate(object value) => $"crypto::scrypt::generate({value})";
+        public static string Compare(string hash, string plainText)
+            => $"crypto::scrypt::compare('{hash}', '{plainText}')";
+
+        public static string Generate(string value)
+            => $"crypto::scrypt::generate('{value}')";
     }
 }
