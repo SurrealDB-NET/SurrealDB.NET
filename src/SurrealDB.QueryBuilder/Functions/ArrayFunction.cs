49afa0759f0135ecadf8abbb1056f25994b21c1e
alexy4744 <alexy4744@gmail.com>
2022-12-29 22:13:17 -0500

    use IEnumerable for ArrayFunction param types

--- src/SurrealDB.QueryBuilder/Functions/ArrayFunction.cs
+++ src/SurrealDB.QueryBuilder/Functions/ArrayFunction.cs
@@ -4,42 +4,42 @@ namespace SurrealDB.QueryBuilder.Functions;
 
 public static class ArrayFunction
 {
-    public static string Combine(ICollection<object> array1, ICollection<dynamic> array2)
+    public static string Combine(IEnumerable<object> array1, IEnumerable<object> array2)
         => $"array::combine([{string.Join(", ", array1)}], [{string.Join(", ", array2)}])";
 
-    public static string Concat(ICollection<object> array1, ICollection<object> array2)
+    public static string Concat(IEnumerable<object> array1, IEnumerable<object> array2)
         => $"array::concat([{string.Join(", ", array1)}], [{string.Join(", ", array2)}])";
 
-    public static string Difference(ICollection<object> array1, ICollection<object> array2)
+    public static string Difference(IEnumerable<object> array1, IEnumerable<object> array2)
         => $"array::difference([{string.Join(", ", array1)}], [{string.Join(", ", array2)}])";
 
-    public static string Distinct(ICollection<object> array)
+    public static string Distinct(IEnumerable<object> array)
         => $"array::distinct([{string.Join(", ", array)}])";
 
-    public static string Intersect(ICollection<object> array1, ICollection<object> array2)
+    public static string Intersect(IEnumerable<object> array1, IEnumerable<object> array2)
         => $"array::intersect([{string.Join(", ", array1)}], [{string.Join(", ", array2)}])";
 
-    public static string Len(ICollection<object> array)
+    public static string Len(IEnumerable<object> array)
         => $"array::len([{string.Join(", ", array)}])";
 
-    public static string Sort(ICollection<object> array)
+    public static string Sort(IEnumerable<object> array)
         => $"array::sort([{string.Join(", ", array)}])";
 
-    public static string Sort(ICollection<object> array, bool isAscending)
+    public static string Sort(IEnumerable<object> array, bool isAscending)
         => $"array::sort([{string.Join(", ", array)}], {isAscending.ToString().ToLower()})";
 
-    public static string Sort(ICollection<object> array, SortOrder sortOrder)
+    public static string Sort(IEnumerable<object> array, SortOrder sortOrder)
         => $"array::sort([{string.Join(", ", array)}], {sortOrder.ToString().ToLower()})";
 
     public static class SortDirection
     {
-        public static string Asc(ICollection<object> array)
+        public static string Asc(IEnumerable<object> array)
             => $"array::sort::asc([{string.Join(", ", array)}])";
 
-        public static string Desc(ICollection<object> array)
+        public static string Desc(IEnumerable<object> array)
             => $"array::sort::desc([{string.Join(", ", array)}])";
     }
 
-    public static string Union(ICollection<object> array1, ICollection<object> array2)
+    public static string Union(IEnumerable<object> array1, IEnumerable<object> array2)
         => $"array::union([{string.Join(", ", array1)}], [{string.Join(", ", array2)}])";
 }
