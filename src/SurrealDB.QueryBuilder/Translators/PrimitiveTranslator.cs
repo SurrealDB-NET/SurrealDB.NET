8c7c76b176e6ff82e7bc117e7bd2e7bdc50a70a5
Juke-Duke <youssefelshabasy8771@gmail.com>
2023-01-02 18:19:12 -0500

    Translator for Primitive types

--- src/SurrealDB.QueryBuilder/Translators/PrimitiveTranslator.cs
+++ src/SurrealDB.QueryBuilder/Translators/PrimitiveTranslator.cs
@@ -1,13 +1,18 @@
+using System.Collections;
+using System.Numerics;
+using SurrealDB.QueryBuilder.DataModels;
+using SurrealDB.QueryBuilder.DataModels.Geometry;
+
 namespace SurrealDB.QueryBuilder.Translators;
 
-using System.Numerics;
-using DataModels;
-
 /// <summary>
-/// Translates a value to a string that matches the SurrealQL syntax,
+/// Translates a value to a string that matches the SurrealQL syntax.
 /// </summary>
 internal static class PrimitiveTranslator
 {
+    internal static string Translate(None none)
+        => $"{none}";
+
     internal static string Translate(bool @bool)
         => @bool ? "true" : "false";
 
@@ -20,6 +25,15 @@ internal static class PrimitiveTranslator
     internal static string Translate(string @string)
         => $"\"{@string}\"";
 
-    internal static string Translate(None none)
-        => "none";
+    internal static string Translate(Duration duration)
+        => $"\"{duration}\"";
+
+    internal static string Translate(DateTime dateTime)
+        => $"\"{dateTime.ToString("o")}\"";
+
+    internal static string Translate(DateTimeOffset dateTimeOffset)
+        => $"\"{dateTimeOffset.ToString("o")}\"";
+
+    internal static string Translate(IGeometry geometry)
+        => $"\"{geometry}\"";
 }
