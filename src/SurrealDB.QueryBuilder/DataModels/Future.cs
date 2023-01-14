84741c3cd926ca1ad0d73fafd40be7695feecead
Juke-Duke <youssefelshabasy8771@gmail.com>
2023-01-13 21:05:15 -0500

    Temporary implementation

--- src/SurrealDB.QueryBuilder/DataModels/Future.cs
+++ src/SurrealDB.QueryBuilder/DataModels/Future.cs
@@ -7,7 +7,7 @@ public sealed class Future<TResult> : IFuture
     private string Translated { get; }
 
     internal Future(TResult value)
-        => Translated = ObjectTranslator.Translate(value);
+        => Translated = $"{{{ObjectTranslator.Translate(value)}}}";
 
     public override string ToString()
         => $"{Translated}";
